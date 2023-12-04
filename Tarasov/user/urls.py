6d1fa5aa56d92eec303c90a4705d74e0b5a80ed4
DenisLukianov21 <DenisLikianov2001@yandex.ru>
2023-12-04 18:06:09 +0300

    registration

---
+++ Tarasov/user/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.views import LogoutView, LoginView
+from django.urls import path
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('signup/', views.signup.as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='user/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(template_name='user/logged_out.html'),
+         name='logout'),
+
+]
